cd34c47e07cc658a838fc03b55d42f658a75b952
nawan936 <gunawansaja697@gmail.com>
2025-03-15 20:52:38 +0700

    Add files via upload

---
+++ kontak.js
@@ -0,0 +1,16 @@
+feather.replace();
+// Toggle class active untuk navbar
+const navbarNav = document.querySelector(".nav");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerMenu.addEventListener("click", (e) => {
+    e.preventDefault(); // Mencegah halaman reload ke home
+    navbarNav.classList.toggle("active");
+});
+
+// Klik di luar sidebar untuk menutup navbar
+document.addEventListener("click", function (e) {
+    if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
